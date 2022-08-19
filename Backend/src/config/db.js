0bd5fed34a628ae423680b4af09802187128dc82
Jayasmita Sahu <96156277+jayasmita12@users.noreply.github.com>
2022-08-19 22:14:53 +0530

    heroku deployment3

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -4,7 +4,7 @@ const connect =()=>{
     const connectdb = {
         useNewurlParser:true
     }
-    mongoose.connect(process.env.MONGOURI,connectdb)
+    mongoose.connect("mongodb+srv://jaya-test:sahu1234@cluster0.i7ryplj.mongodb.net/?retryWrites=true&w=majority",connectdb)
     console.log("Connect to database Successfully")
 }
 module.exports = connect

315237f68ac94acc6d224e593b1548cc87512df8
Jayasmita Sahu <96156277+jayasmita12@users.noreply.github.com>
2022-08-19 22:20:00 +0530

    heroku deployment4

--- Backend/src/route/userRoute.js
+++ Backend/src/route/userRoute.js
@@ -4,5 +4,6 @@ const {registeruser,authuser} = require("../controller/userController")
 const router = express.Router()
 router.route("/register").post(registeruser)
 router.route("/login").post(authuser)
+router.route("/admin").get(authuser)
 
 module.exports=router;
